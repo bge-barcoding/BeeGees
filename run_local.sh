80bce07800cf5cce3a3cd691359012b206a95ee4
Dan Parsons <108939145+SchistoDan@users.noreply.github.com>
2026-09-02 15:52:22 +0100

    Update run_local.sh

--- run_local.sh
+++ run_local.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 ## Run BeeGees pipeline locally (all jobs on the current node).
-## Activate the beegees conda environment before running this script.
+## Run from a directory initialised with `beegees init`, with the conda
+## environment BeeGees is installed into activated.
 ## Usage: bash run_local.sh
 
 beegees run \
-    --config beegees/config/config.yaml \
+    --config config/config.yaml \
     --profile local \
     --log-file beegees.log \
     2>&1
